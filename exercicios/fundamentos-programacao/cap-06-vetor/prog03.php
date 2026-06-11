7f147b3a8eaa067e42954a493729c5fc84e6fab4
bentogomes19 <bento.gomes@yandex.com>
2026-06-10 23:08:02 -0300

    practice makes perfect

--- exercicios/fundamentos-programacao/cap-06-vetor/prog03.php
+++ exercicios/fundamentos-programacao/cap-06-vetor/prog03.php
@@ -1,1 +1,19 @@
 <?php
+
+
+$vetor1 = [3, 5, 4, 2, 2, 5, 3, 2, 5, 9];
+$vetor2 = [7, 15, 20, 0, 18, 4, 55, 23, 8, 6];
+
+$vetorResultante = [];
+
+$j = 0;
+for ($i = 0; $i <= 9; $i++) {
+    $vetorResultante[$j] = $vetor1[$i];
+    $j++;
+    $vetorResultante[$j] = $vetor2[$i];
+    $j++;
+}
+
+for ($i = 0; $i < 20; $i++) {
+    echo $vetorResultante[$i] . ' | ';
+}
--- exercicios/fundamentos-programacao/cap-06-vetor/prog04.php
+++ exercicios/fundamentos-programacao/cap-06-vetor/prog04.php
@@ -1,1 +1,36 @@
 <?php
+
+
+$vetor          = [];
+$vetorPositivos = [];
+$cont_positivos = 0;
+$cont_negativos = 0;
+
+for ($i = 0; $i < 8; $i++) {
+    $vetor[$i] = (int)(trim(readline("Numero: ")));
+
+    /* Verificando se o número é positivo ou negativo */
+    if ($vetor[$i] >= 0) {
+        $vetorPositivos[$cont_positivos] = $vetor[$i];
+        $cont_positivos++;
+    } else {
+        $vetorNegativos[$cont_negativos] = $vetor[$i];
+        $cont_negativos++;
+    }
+}
+
+if ($cont_negativos === 0) {
+    echo "Vetor de negativos vazios." . PHP_EOL;
+} else {
+    for ($i = 0; $i < $cont_negativos ; $i++) {
+        echo $vetorNegativos[$i] . " | ";
+    }
+}
+
+if ($cont_positivos === 0) {
+    echo "Vetor de negativos vazios." . PHP_EOL;
+} else {
+    for ($i = 0; $i < $cont_positivos; $i++) {
+        echo $vetorPositivos[$i] . " | ";
+    }
+}
